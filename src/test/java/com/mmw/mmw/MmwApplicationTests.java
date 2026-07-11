d95c240f3f50b7344ceaa352b8b4a4cdebfffc91
juzeo <65641506+juzeo@users.noreply.github.com>
2026-07-11 16:21:12 +0900

    chore: initialize spring boot project with gradle Configure Spring Boot 4.0.7 and Java 25.0.3

---
+++ src/test/java/com/mmw/mmw/MmwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmw.mmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
